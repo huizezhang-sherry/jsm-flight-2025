1541edacd9fbba9bc2b7531e11e3101423fa2655
Sarah M. Coleman <sarahcol@utexas.edu>
2025-05-08 15:15:13 -0500

    smc add data

---
+++ scripts/04-SMC_passengerData.R
@@ -0,0 +1,8 @@
+
+# AIRPORT/AIRLINE SPECIFIC::: (and departure/arrival)
+# https://www.transtats.bts.gov/Data_Elements.aspx
+
+# DATA ACTUALLY USED:
+# https://www.transtats.bts.gov/Tables.asp?6noyr_vq=FJH
+# name: Air Carrier Summary : T1: U.S. Air Carrier Traffic And Capacity Summary by Service Class
+# https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FJH&QO_fu146_anzr=Nv4%20Pn44vr4%20f7zzn4B
