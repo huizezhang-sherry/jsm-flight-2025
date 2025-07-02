0d88aa985479f1f11891c09e86a7b0d55d530004
lydialucchesi <lyd.lucchesi@gmail.com>
2025-07-02 13:19:21 -0500

    Add 2017 pairs

---
+++ scripts/12-LL-select_20_pairs.R
@@ -0,0 +1,41 @@
+library(arrow)
+library(dplyr)
+
+y <- 2017
+
+# import passenger count data
+pv <- read.csv(paste0(getwd(), "/unzipped/T_T100D_SEGMENT_ALL_CARRIER_", y, ".csv"))
+pv <- subset(pv, subset = UNIQUE_CARRIER %in% c("AA"),
+             select = c("UNIQUE_CARRIER", "ORIGIN", "QUARTER", "PASSENGERS"))
+colnames(pv) <- c("airline", "origin", "quarter", "passengers")
+# get yearly passenger counts for airline/airport pairs
+pvYear <- pv %>%
+  group_by(airline, origin) %>%
+  summarise(count = sum(passengers)) %>%
+  as.data.frame()
+
+# import flight data
+flights <- read_parquet(paste0("Year=", y, "/data_0.parquet"))
+flights <- subset(flights, Reporting_Airline %in% c("AA"))
+uniPairs <- unique(flights[ ,c("Reporting_Airline", "Origin")])
+# get unique airline/airport pairs
+colnames(uniPairs) <- c("airline", "origin")
+
+# merge flights pairs with yearly passenger counts
+df <- merge(uniPairs, pvYear)
+
+# group into quartiles (for yearly passenger counts), by airline
+dfQ <- df %>%
+  group_by(airline) %>%
+  mutate(quantile = ntile(count, 4)) %>%
+  ungroup()
+
+# sort by airline and yearly passenger count (so it is in descending order)
+# and then take the top five airports from each airline quartile
+df20 <- dfQ %>%
+  group_by(airline, quantile) %>%
+  arrange(desc(count)) %>%
+  slice_head(n = 5)
+
+colnames(df20) <- c("airline", "origin", "passenger_count", "quartile")
+write.csv(df20, "pairs20.csv", row.names = FALSE)
