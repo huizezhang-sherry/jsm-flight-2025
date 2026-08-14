1ee07d5f56fde4952eeb951bc85ba05ffb2b7c36
Sherry Zhang <huizezhangsh@gmail.com>
2026-08-14 11:58:46 -0500

    revising figure 2 and 3

--- scripts/fig2.R
+++ scripts/fig2.R
@@ -40,10 +40,13 @@ two_df <- flight_df_raw |>
   mutate(n = ifelse(type == "dep", n, -n)) |>
   mutate(type = factor(type, levels = c('dep', 'arr'), labels = c("dep" = 'Departure', "arr" = 'Arrival'))) |>
    mutate(airline_airport = factor(airline_airport,
-                                  labels = c("American / Austin-Bergstrom International Airport (AUS)",
-                                             "American / Dallas/Fort Worth International Airport (DFW)")))
+                                  labels = c("Austin-Bergstrom International Airport (AUS)",
+                                             "Dallas/Fort Worth International Airport (DFW)")))
 
-write_csv(two_df, file = here::here("data/two_df.csv"))
+#write_csv(two_df, file = here::here("data/two_df.csv"))
+
+
+# ggsave(filename = "figures/fig2.png", height = 10, width = 18, unit = "cm", bg = "white")
 
 # Plot aesthetics
 color_list <- c("Arrival" = "#00a9b7", "Departure" = "#353F47")
@@ -58,135 +61,149 @@ dfwtitle <- 'American / Dallas/Fort Worth International Airport (DFW)'
 xlab <- "Binned time (10 minute intervals)"
 ylab <- "Annual Flight Count"
 
-# Function to color y axis labels in new plots
-color_y_axis_labels <- function(p,
-                                breaks,
-                                neg = "#00a9b7",
-                                pos = "#353F47",
-                                zero = "black") {
-
-  gt <- ggplotGrob(p)
-
-  axis_id <- which(gt$layout$name == "axis-l")
-
-  axis_grob <- gt$grobs[[axis_id]]
-
-  find_text_grob <- function(g) {
-
-    if (inherits(g, "text")) return(g)
-
-    if (!is.null(g$children)) {
-      for (child in g$children) {
-        out <- find_text_grob(child)
-        if (!is.null(out)) return(out)
-      }
-    }
-
-    if (!is.null(g$grobs)) {
-      for (child in g$grobs) {
-        out <- find_text_grob(child)
-        if (!is.null(out)) return(out)
-      }
-    }
-
-    NULL
-  }
-
-  replace_text_grob <- function(g, new_text) {
-
-    if (inherits(g, "text")) {
-      return(new_text)
-    }
-
-    if (!is.null(g$children)) {
-      for (i in seq_along(g$children)) {
-        g$children[[i]] <-
-          replace_text_grob(g$children[[i]], new_text)
-      }
-    }
-
-    if (!is.null(g$grobs)) {
-      for (i in seq_along(g$grobs)) {
-        g$grobs[[i]] <-
-          replace_text_grob(g$grobs[[i]], new_text)
-      }
-    }
-
-    g
-  }
-
-  txt <- find_text_grob(axis_grob)
-
-  if (length(txt$label) != length(breaks)) {
-    stop("Number of breaks does not match number of axis labels.")
-  }
-
-  txt$gp$col <- ifelse(
-    breaks < 0, neg,
-    ifelse(breaks > 0, pos, zero)
-  )
-
-  axis_grob <- replace_text_grob(axis_grob, txt)
-
-  gt$grobs[[axis_id]] <- axis_grob
-
-  gt
-}
-
-# The new plot (reviewer comments)
-aus <- two_df |>
-  filter(airport == 'AUS') |>
+two_df |>
   ggplot(aes(x = block, y = n, color = type, fill = type)) +
   geom_col() +
   scale_x_datetime(labels = xlabels, breaks = xbreaks) +
-  scale_y_continuous(labels = auslabels, breaks = ausbreaks) +
-  scale_fill_manual(name = "Flight type", values = color_list) +
-  scale_color_manual(name = "Flight type", values = color_list) +
-  labs(title = austitle) +
-  theme_minimal(base_size = 10) +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        plot.title = element_text(hjust = 0.5, size = 8))
-
-dfw <- two_df |>
-  filter(airport == 'DFW') |>
-  ggplot(aes(x = block, y = n, color = type, fill = type)) +
-  geom_col() +
-  scale_x_datetime(labels = xlabels, breaks = xbreaks) +
-  scale_y_continuous(labels = dfwlabels, breaks = dfwbreaks) +
-  scale_fill_manual(name = "Flight type", values = color_list) +
-  scale_color_manual(name = "Flight type", values = color_list) +
-  labs(title = dfwtitle) +
+  scale_y_continuous(labels = abs, limits = \(x) c(-max(abs(x)), max(abs(x)))) + 
+  scale_fill_manual(name = "Flight type", values = color_list, breaks = c("Arrival", "Departure")) +
+  scale_color_manual(name = "Flight type", values = color_list, breaks = c("Arrival", "Departure")) +
+  facet_wrap(vars(airline_airport), ncol = 1, scale = "free") + 
   theme_minimal(base_size = 10) +
   theme(legend.position = "bottom",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.title = element_text(size = 10),
-        plot.title = element_text(hjust = 0.5, size = 8)) +
-  coord_cartesian(ylim = c(-4000, 4000))
+        plot.title = element_text(hjust = 0.5, size = 8)) + 
+  ylab("Annual Flight Count") + 
+  xlab("Binned time (10 minute intervals)")
 
-dfw_legend <- cowplot::get_legend(dfw)
+# # Function to color y axis labels in new plots
+# color_y_axis_labels <- function(p,
+#                                 breaks,
+#                                 neg = "#00a9b7",
+#                                 pos = "#353F47",
+#                                 zero = "black") {
 
-aus_col <- color_y_axis_labels(aus, ausbreaks)
-dfw_col <- color_y_axis_labels((dfw + theme(legend.position = 'none')), dfwbreaks)
+#   gt <- ggplotGrob(p)
 
-plots <- gridExtra::arrangeGrob(aus_col, dfw_col, ncol = 1)
+#   axis_id <- which(gt$layout$name == "axis-l")
 
-# Create shared x/y axis title
-x_title <- gridtext::richtext_grob(xlab, gp = gpar(fontsize = 10))
-y_title <- gridtext::richtext_grob(ylab, rot = 90, gp = gpar(fontsize = 10))
+#   axis_grob <- gt$grobs[[axis_id]]
 
-# Add labels around the combined plots
-combined <- gridExtra::arrangeGrob(plots, left = y_title, bottom = x_title)
-final <- gridExtra::arrangeGrob(combined, bottom = dfw_legend)
+#   find_text_grob <- function(g) {
 
-# Finally save this!
-ggsave(plot = final, filename = "figures/fig2.png",
-       height = 10, width = 18, unit = "cm", bg = "white")
+#     if (inherits(g, "text")) return(g)
 
-# Get the total number of arrivals and departure counts (for main text)
-arr_dep_sum <- two_df |>
-  mutate(n = abs(n)) |>
-  group_by(airline, airport) |>
-  summarise(total_flights = sum(n), .groups = 'drop')
+#     if (!is.null(g$children)) {
+#       for (child in g$children) {
+#         out <- find_text_grob(child)
+#         if (!is.null(out)) return(out)
+#       }
+#     }
+
+#     if (!is.null(g$grobs)) {
+#       for (child in g$grobs) {
+#         out <- find_text_grob(child)
+#         if (!is.null(out)) return(out)
+#       }
+#     }
+
+#     NULL
+#   }
+
+#   replace_text_grob <- function(g, new_text) {
+
+#     if (inherits(g, "text")) {
+#       return(new_text)
+#     }
+
+#     if (!is.null(g$children)) {
+#       for (i in seq_along(g$children)) {
+#         g$children[[i]] <-
+#           replace_text_grob(g$children[[i]], new_text)
+#       }
+#     }
+
+#     if (!is.null(g$grobs)) {
+#       for (i in seq_along(g$grobs)) {
+#         g$grobs[[i]] <-
+#           replace_text_grob(g$grobs[[i]], new_text)
+#       }
+#     }
+
+#     g
+#   }
+
+#   txt <- find_text_grob(axis_grob)
+
+#   if (length(txt$label) != length(breaks)) {
+#     stop("Number of breaks does not match number of axis labels.")
+#   }
+
+#   txt$gp$col <- ifelse(
+#     breaks < 0, neg,
+#     ifelse(breaks > 0, pos, zero)
+#   )
+
+#   axis_grob <- replace_text_grob(axis_grob, txt)
+
+#   gt$grobs[[axis_id]] <- axis_grob
+
+#   gt
+# }
+
+# # The new plot (reviewer comments)
+# aus <- two_df |>
+#   filter(airport == 'AUS') |>
+#   ggplot(aes(x = block, y = n, color = type, fill = type)) +
+#   geom_col() +
+#   scale_x_datetime(labels = xlabels, breaks = xbreaks) +
+#   scale_y_continuous(labels = auslabels, breaks = ausbreaks) +
+#   scale_fill_manual(name = "Flight type", values = color_list) +
+#   scale_color_manual(name = "Flight type", values = color_list) +
+#   labs(title = austitle) +
+#   theme_minimal(base_size = 10) +
+#   theme(legend.position = "none",
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank(),
+#         plot.title = element_text(hjust = 0.5, size = 8))
+
+# dfw <- two_df |>
+#   filter(airport == 'DFW') |>
+#   ggplot(aes(x = block, y = n, color = type, fill = type)) +
+#   geom_col() +
+#   scale_x_datetime(labels = xlabels, breaks = xbreaks) +
+#   scale_y_continuous(labels = dfwlabels, breaks = dfwbreaks) +
+#   scale_fill_manual(name = "Flight type", values = color_list) +
+#   scale_color_manual(name = "Flight type", values = color_list) +
+#   labs(title = dfwtitle) +
+#   theme_minimal(base_size = 10) +
+#   theme(legend.position = "bottom",
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank(),
+#         legend.title = element_text(size = 10),
+#         plot.title = element_text(hjust = 0.5, size = 8)) +
+#   coord_cartesian(ylim = c(-4000, 4000))
+
+# dfw_legend <- cowplot::get_legend(dfw)
+
+# aus_col <- color_y_axis_labels(aus, ausbreaks)
+# dfw_col <- color_y_axis_labels((dfw + theme(legend.position = 'none')), dfwbreaks)
+
+# plots <- gridExtra::arrangeGrob(aus_col, dfw_col, ncol = 1)
+
+# # Create shared x/y axis title
+# x_title <- gridtext::richtext_grob(xlab, gp = gpar(fontsize = 10))
+# y_title <- gridtext::richtext_grob(ylab, rot = 90, gp = gpar(fontsize = 10))
+
+# # Add labels around the combined plots
+# combined <- gridExtra::arrangeGrob(plots, left = y_title, bottom = x_title)
+# final <- gridExtra::arrangeGrob(combined, bottom = dfw_legend)
+
+# # Finally save this!
+# ggsave(plot = final, filename = "figures/fig2.png",
+#        height = 10, width = 18, unit = "cm", bg = "white")
+
+# # Get the total number of arrivals and departure counts (for main text)
+# arr_dep_sum <- two_df |>
+#   mutate(n = abs(n)) |>
+#   group_by(airline, airport) |>
+#   summarise(total_flights = sum(n), .groups = 'drop')
--- scripts/fig3.R
+++ scripts/fig3.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(arrow)
 library(cowplot)
+library(usmap)
+library(sf)
+library(patchwork)
 
 st_fips <- sprintf("%02d", c(1, 3:14, 16:56)) # no AK, HI
 
@@ -12,22 +15,22 @@ flight_df_raw <- read_parquet("Year=2017/data_0.parquet") |>
 aa_hubs <- c("DFW", "CLT", "ORD", "PHL", "LGA", "LAX", "DCA", "AUS", "ATL", "PDX", "EGE", "ALB", "OAK", "AMA")
 airport_df <- tibble(
   aa_hubs=aa_hubs,
-  airline_airport = c(
-    "American / Dallas/Fort Worth International Airport (DFW)",
-    "American / Charlotte Douglas International Airport (CLT)",
-    "American / Chicago O'Hare International Airport (ORD)",
-    "American / Philadelphia International Airport (PHL)",
-    "American / LaGuardia Airport (LGA)",
-    "American / Los Angeles International Airport (LAX)",
-    "American / Ronald Reagan Washington National Airport (DCA)",
-    "American / Austin-Bergstrom International Airport (AUS)",
-    "American / Hartsfield-Jackson Atlanta International Airport (ATL)",
-    "American / Portland International Airport (PDX)",
-    "American / Eagle County Regional Airport (EGE)",
-    "American / Albany International Airport (ALB)",
-    "American / Oakland International Airport (OAK)",
-     "American / Rick Husband Amarillo International Airport (AMA)"
-  ))
+airline_airport = c(
+  "Dallas/Fort Worth International Airport, DFW (Dallas, TX)",
+  "Charlotte Douglas International Airport, CLT (Charlotte, NC)",
+  "Chicago O'Hare International Airport, ORD (Chicago, IL)",
+  "Philadelphia International Airport, PHL (Philadelphia, PA)",
+  "LaGuardia Airport, LGA (New York, NY)",
+  "Los Angeles International Airport, LAX (Los Angeles, CA)",
+  "Ronald Reagan Washington National Airport, DCA (Washington, DC)",
+  "Austin-Bergstrom International Airport, AUS (Austin, TX)",
+  "Hartsfield-Jackson Atlanta International Airport, ATL (Atlanta, GA)",
+  "Portland International Airport, PDX (Portland, OR)",
+  "Eagle County Regional Airport, EGE (Vail, CO)",
+  "Albany International Airport, ALB (Albany, NY)",
+  "Oakland International Airport, OAK (Oakland, CA)",
+  "Rick Husband Amarillo International Airport, AMA (Amarillo, TX)"
+))
 
 summarize_count <- function(data, block_size = 10, airports){
   data |>
@@ -61,179 +64,201 @@ aa_df <- flight_df_raw |>
   left_join(airport_df, by = c("airport" = "aa_hubs")) |>
   mutate(airline_airport = factor(airline_airport, levels = airport_df$airline_airport))
 
-write_csv(aa_df, file = here::here("data/aa_df.csv"))
+#write_csv(aa_df, file = here::here("data/aa_df.csv"))
+
 
 color_list <- c("Arrival" = "#00a9b7", "Departure" = "#353F47")
+xlabels <- paste0(seq(0, 24, 4), ':00')
+xbreaks <- ymd_hm(paste0('2017-01-01 ', xlabels))
 
-fig3 <- aa_df |>
+p1 <- aa_df |>
   ggplot(aes(x = block, y = n, color = type, fill = type)) +
   geom_col() +
-  scale_x_datetime(date_labels =  "%H:%M", date_breaks = "4 hour") +
+  scale_x_datetime(labels = xlabels, breaks = xbreaks) +
+  scale_y_continuous(labels = abs, limits = \(x) c(-max(abs(x)), max(abs(x)))) + 
   theme_minimal() +
-  xlab("Time of the date") +
-  ylab("Count") +
   facet_wrap(vars(airline_airport), scales = "free_y", ncol = 2, dir = "v") +
-  scale_fill_manual(name = "Flight type", values = color_list) +
-  scale_color_manual(name = "Flight type", values = color_list) +
+  scale_fill_manual(name = "Flight type", values = color_list, breaks = c("Arrival", "Departure")) +
+  scale_color_manual(name = "Flight type", values = color_list, breaks = c("Arrival", "Departure")) +
   xlab("Binned time (10 minute intervals)") +
   ylab("Annual Flight Count") +
   theme(legend.position = "bottom", text = element_text(colour = "black", size = 10))
 
-# Function to color y axis labels in new plots
-color_y_axis_labels <- function(p,
-                                breaks,
-                                neg = "#00a9b7",
-                                pos = "#353F47",
-                                zero = "black") {
+# # try to add a map but it doesn't look well
+# airports <- read_csv('data/airports_in_cont_us.csv')
+# airports_aa <- airports |> right_join(airport_df, by = c("airport" = "aa_hubs"))
+# us_map_sf <-  us_map(regions = 'states') |> filter(!abbr %in% c("AK", "HI", "PR"))
+# p2 <- ggplot() +
+#   geom_sf(data = us_map_sf, color = "grey70", fill = "grey90") +
+#   geom_point(data = airports_aa, aes(x = x, y = y), color = '#005f86') +
+#   geom_label(data = airports_aa, aes(x = x, y = y - 50000, label = airport), color = '#005f86') +
+#   theme_void() +
+#   scale_size_continuous(name = "Number of Flights", labels = scales::label_comma()) +
+#   theme(legend.position = "bottom",text = element_text(colour = "black", size = 10))
 
-  gt <- ggplotGrob(p)
+# p <- (p1 / p2) + plot_layout(heights = c(3, 1))
+# p
 
-  axis_id <- which(gt$layout$name == "axis-l")
-
-  axis_grob <- gt$grobs[[axis_id]]
-
-  find_text_grob <- function(g) {
-
-    if (inherits(g, "text")) return(g)
-
-    if (!is.null(g$children)) {
-      for (child in g$children) {
-        out <- find_text_grob(child)
-        if (!is.null(out)) return(out)
-      }
-    }
-
-    if (!is.null(g$grobs)) {
-      for (child in g$grobs) {
-        out <- find_text_grob(child)
-        if (!is.null(out)) return(out)
-      }
-    }
-
-    NULL
-  }
-
-  replace_text_grob <- function(g, new_text) {
-
-    if (inherits(g, "text")) {
-      return(new_text)
-    }
-
-    if (!is.null(g$children)) {
-      for (i in seq_along(g$children)) {
-        g$children[[i]] <-
-          replace_text_grob(g$children[[i]], new_text)
-      }
-    }
-
-    if (!is.null(g$grobs)) {
-      for (i in seq_along(g$grobs)) {
-        g$grobs[[i]] <-
-          replace_text_grob(g$grobs[[i]], new_text)
-      }
-    }
-
-    g
-  }
-
-  txt <- find_text_grob(axis_grob)
-
-  if (length(txt$label) != length(breaks)) {
-    stop("Number of breaks does not match number of axis labels.")
-  }
-
-  txt$gp$col <- ifelse(
-    breaks < 0, neg,
-    ifelse(breaks > 0, pos, zero)
-  )
-
-  axis_grob <- replace_text_grob(axis_grob, txt)
-
-  gt$grobs[[axis_id]] <- axis_grob
-
-  gt
-}
-
-plot_formatted <- function(df, air){
-
-  xlabels <- paste0(seq(0, 24, 4), ':00')
-  xbreaks <- ymd_hm(paste0('2017-01-01 ', xlabels))
-
-  df_filtered <- df |>
-    filter(airport == air)
-
-  ymax <- df_filtered |>
-    pull(n) |>
-    abs() |>
-    max()
-
-  if (ymax > 1800){
-    yupper <- ceiling(ymax/1000)*1000
-  } else if (ymax > 1000){
-    yupper <- ceiling(ymax/200)*200
-  } else if (ymax > 200){
-    yupper <- ceiling(ymax/100)*100
-  } else {
-    yupper <- ceiling(ymax/20)*20
-  }
-
-  ybreaks <- seq(-yupper, yupper, length.out = 5)
-  ylabels <- abs(ybreaks)
-
-  title <- df_filtered |>
-    pull(airline_airport) |>
-    unique()
-
-  p <- df_filtered |>
-    ggplot(aes(x = block, y = n, color = type, fill = type)) +
-    geom_col() +
-    scale_x_datetime(labels = xlabels, breaks = xbreaks) +
-    scale_y_continuous(labels = ylabels, breaks = ybreaks) +
-    scale_fill_manual(name = "Flight type", values = color_list) +
-    scale_color_manual(name = "Flight type", values = color_list) +
-    labs(title = title) +
-    theme_minimal(base_size = 10) +
-    theme(legend.position = "none",
-          axis.title.x = element_blank(),
-          axis.title.y = element_blank(),
-          plot.title = element_text(hjust = 0.5, size = 8)) +
-    coord_cartesian(ylim = c(-yupper, yupper))
-  return(color_y_axis_labels(p, breaks = ybreaks))
-}
-
-aa_hubs <- c("DFW", "CLT", "ORD", "PHL", "LGA", "LAX", "DCA", "AUS", "ATL", "PDX", "EGE", "ALB", "OAK", "AMA")
-
-xlab <- "Binned time (10 minute intervals)"
-ylab <- "Annual Flight Count"
-
-# Create shared x/y axis title
-x_title <- gridtext::richtext_grob(xlab, gp = gpar(fontsize = 10))
-y_title <- gridtext::richtext_grob(ylab, rot = 90, gp = gpar(fontsize = 10))
-
-plots_list <- lapply(aa_hubs, function(hub){
-  plot_formatted(aa_df, hub)
-})
-
-combined <- gridExtra::arrangeGrob(
-  grobs = plots_list,
-  as.table = FALSE,
-  ncol = 2,
-  nrow = 7,
-  left = y_title,
-  bottom = x_title)
-
-legend <- cowplot::get_legend(fig3)
-
-final <- gridExtra::arrangeGrob(
-  combined,
-  legend,
-  ncol = 1,
-  heights = c(1, 0.08)
-)
-
-grid::grid.newpage()
-grid::grid.draw(final)
-
-ggsave(plot = final,
+ggsave(plot = p1,
        filename = "figures/fig3.png",
-       height = 18, width = 18, unit = "cm", bg = "white")
+       height = 20, width = 20, unit = "cm", bg = "white")
+  
+
+# # Function to color y axis labels in new plots
+# color_y_axis_labels <- function(p,
+#                                 breaks,
+#                                 neg = "#00a9b7",
+#                                 pos = "#353F47",
+#                                 zero = "black") {
+
+#   gt <- ggplotGrob(p)
+
+#   axis_id <- which(gt$layout$name == "axis-l")
+
+#   axis_grob <- gt$grobs[[axis_id]]
+
+#   find_text_grob <- function(g) {
+
+#     if (inherits(g, "text")) return(g)
+
+#     if (!is.null(g$children)) {
+#       for (child in g$children) {
+#         out <- find_text_grob(child)
+#         if (!is.null(out)) return(out)
+#       }
+#     }
+
+#     if (!is.null(g$grobs)) {
+#       for (child in g$grobs) {
+#         out <- find_text_grob(child)
+#         if (!is.null(out)) return(out)
+#       }
+#     }
+
+#     NULL
+#   }
+
+#   replace_text_grob <- function(g, new_text) {
+
+#     if (inherits(g, "text")) {
+#       return(new_text)
+#     }
+
+#     if (!is.null(g$children)) {
+#       for (i in seq_along(g$children)) {
+#         g$children[[i]] <-
+#           replace_text_grob(g$children[[i]], new_text)
+#       }
+#     }
+
+#     if (!is.null(g$grobs)) {
+#       for (i in seq_along(g$grobs)) {
+#         g$grobs[[i]] <-
+#           replace_text_grob(g$grobs[[i]], new_text)
+#       }
+#     }
+
+#     g
+#   }
+
+#   txt <- find_text_grob(axis_grob)
+
+#   if (length(txt$label) != length(breaks)) {
+#     stop("Number of breaks does not match number of axis labels.")
+#   }
+
+#   txt$gp$col <- ifelse(
+#     breaks < 0, neg,
+#     ifelse(breaks > 0, pos, zero)
+#   )
+
+#   axis_grob <- replace_text_grob(axis_grob, txt)
+
+#   gt$grobs[[axis_id]] <- axis_grob
+
+#   gt
+# }
+
+# plot_formatted <- function(df, air){
+
+#   xlabels <- paste0(seq(0, 24, 4), ':00')
+#   xbreaks <- ymd_hm(paste0('2017-01-01 ', xlabels))
+
+#   df_filtered <- df |>
+#     filter(airport == air)
+
+#   ymax <- df_filtered |>
+#     pull(n) |>
+#     abs() |>
+#     max()
+
+#   if (ymax > 1800){
+#     yupper <- ceiling(ymax/1000)*1000
+#   } else if (ymax > 1000){
+#     yupper <- ceiling(ymax/200)*200
+#   } else if (ymax > 200){
+#     yupper <- ceiling(ymax/100)*100
+#   } else {
+#     yupper <- ceiling(ymax/20)*20
+#   }
+
+#   ybreaks <- seq(-yupper, yupper, length.out = 5)
+#   ylabels <- abs(ybreaks)
+
+#   title <- df_filtered |>
+#     pull(airline_airport) |>
+#     unique()
+
+#   p <- df_filtered |>
+#     ggplot(aes(x = block, y = n, color = type, fill = type)) +
+#     geom_col() +
+#     scale_x_datetime(labels = xlabels, breaks = xbreaks) +
+#     scale_y_continuous(labels = ylabels, breaks = ybreaks) +
+#     scale_fill_manual(name = "Flight type", values = color_list) +
+#     scale_color_manual(name = "Flight type", values = color_list) +
+#     labs(title = title) +
+#     theme_minimal(base_size = 10) +
+#     theme(legend.position = "none",
+#           axis.title.x = element_blank(),
+#           axis.title.y = element_blank(),
+#           plot.title = element_text(hjust = 0.5, size = 8)) +
+#     coord_cartesian(ylim = c(-yupper, yupper))
+#   return(color_y_axis_labels(p, breaks = ybreaks))
+# }
+
+# aa_hubs <- c("DFW", "CLT", "ORD", "PHL", "LGA", "LAX", "DCA", "AUS", "ATL", "PDX", "EGE", "ALB", "OAK", "AMA")
+
+# xlab <- "Binned time (10 minute intervals)"
+# ylab <- "Annual Flight Count"
+
+# # Create shared x/y axis title
+# x_title <- gridtext::richtext_grob(xlab, gp = gpar(fontsize = 10))
+# y_title <- gridtext::richtext_grob(ylab, rot = 90, gp = gpar(fontsize = 10))
+
+# plots_list <- lapply(aa_hubs, function(hub){
+#   plot_formatted(aa_df, hub)
+# })
+
+# combined <- gridExtra::arrangeGrob(
+#   grobs = plots_list,
+#   as.table = FALSE,
+#   ncol = 2,
+#   nrow = 7,
+#   left = y_title,
+#   bottom = x_title)
+
+# legend <- cowplot::get_legend(fig3)
+
+# final <- gridExtra::arrangeGrob(
+#   combined,
+#   legend,
+#   ncol = 1,
+#   heights = c(1, 0.08)
+# )
+
+# grid::grid.newpage()
+# grid::grid.draw(final)
+
+# ggsave(plot = final,
+#        filename = "figures/fig3.png",
+#        height = 18, width = 18, unit = "cm", bg = "white")
